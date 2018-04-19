ca33ca0c423ab22c90d6e26543b5bc1a10e28d48
Kar Epker <karepker@gmail.com>
2018-04-18 23:33:27 -0700

    Move aliases from default to personal.

--- .bash.d/aliases_personal.bash
+++ .bash.d/aliases_personal.bash
@@ -3,3 +3,15 @@ alias g++='g++ -Wall -Wextra -Werror -pedantic-errors -std=c++14'
 alias clang++='clang++ -Wall -Wextra -Werror -pedantic-errors -std=c++14'
 
 alias pylint='pylint --extension-pkg-whitelist=numpy'
+
+# Git aliases
+alias gs='git status'  # Take precedence over ghostscript `gs` command.
+alias gb='git branch'
+alias gd='git diff'
+
+# Makes ghostscript callable.
+alias ghostscript='/usr/bin/gs'
+
+# Evil mode is still overrated though.
+alias ed="emacsclient -c -n"
+

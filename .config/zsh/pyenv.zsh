47a851e0a438e53f45f3b1dfaae6dfb71c0fd4de
Kar Epker <karepker@gmail.com>
2020-05-20 23:34:35 -0700

    Add pyenv initialization to zsh config.

---
+++ .config/zsh/pyenv.zsh
@@ -0,0 +1,5 @@
+# Installing manually may require that some pyenv shims directory be appended to
+# path (as rbenv does), but Arch Linux installs to /usr/bin/.
+
+# Required by pyenv.
+eval "$(pyenv init -)"
--- .zshrc
+++ .zshrc
@@ -1,5 +1,6 @@
 # Default settings.
 source ${HOME}/.config/zsh/base.zsh
 
-# For rbenv.
+# For rbenv and pyenv.
 source ${HOME}/.config/zsh/rbenv.zsh
+source ${HOME}/.config/zsh/pyenv.zsh

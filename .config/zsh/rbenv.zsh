ce0599c23093e335a8993263525269a468781589
Kar Epker <karepker@gmail.com>
2023-04-18 23:33:20 -0700

    Do not prepare environment for rbenv if it is not installed.

--- .config/zsh/rbenv.zsh
+++ .config/zsh/rbenv.zsh
@@ -1,5 +1,9 @@
 # rbenv normally requires that ~/.rbenv/bin/ be prepended to PATH, but the AUR
 # version of rbenv installs to /usr/bin/, which is already in PATH.
 
-# Required by rbenv.
-eval "$(rbenv init -)"
+# Only prepare rbenv if rbenv is actually installed.
+# Inspired by https://stackoverflow.com/a/677212.
+if ! command -v rbenv &> /dev/null ; then
+	# Required by rbenv.
+	eval "$(rbenv init -)"
+fi

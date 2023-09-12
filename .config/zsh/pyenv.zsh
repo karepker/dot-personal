8c0de6abcd738785fc356edf06a1e24855eea3bb
Kar Epker <karepker@gmail.com>
2023-09-11 20:52:20 -0700

    Only try to load pyenv if it's actually installed.

--- .config/zsh/pyenv.zsh
+++ .config/zsh/pyenv.zsh
@@ -1,5 +1,9 @@
 # Installing manually may require that some pyenv shims directory be appended to
 # path (as rbenv does), but Arch Linux installs to /usr/bin/.
 
-# Required by pyenv.
-eval "$(pyenv init -)"
+# Only prepare pyenv if pyenv is actually installed.
+# Inspired by https://stackoverflow.com/a/677212.
+if command -v pyenv &> /dev/null ; then
+	# Required by pyenv.
+	eval "$(pyenv init -)"
+fi
